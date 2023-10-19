adc515bf3bd2410b3860b83cb8a5a981253cdc54
Alina74 <shamilevna74@gmail.com>
2023-10-19 12:24:43 +0500

    components: button and input

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,12 +1,38 @@
 import { getPage } from "../../shared/lib/index.js";
+import { Button } from "../../shared/ui/button/index.js";
+import { Input } from "../../shared/ui/input/index.js"
 
 export default () => {
-
+    const TestButton = false
+    const TestInput = false
     return getPage({
         title: "Главная",
         body: `
             <h1>Главная</h1>
-            
+            ${Button({
+            label: "Тест",
+            extraAttrs: {
+                "data-js-require": true,
+                "id": "btn"
+            },
+            extraClasses: {
+                isHidden: true,
+                isDisabled: TestButton,
+            }
+        })}
+            ${Input({
+            label: "Тестовый инпут",
+            extraAttrs: {
+                "data-js-require": true,
+                "id": "inp",
+                "name": "test",
+                "type": "number"
+            },
+            extraClasses: {
+                isHidden: true,
+                isDisabled: TestInput,
+            }
+        })}
             <nav>
                 <ul>
                     <li>
